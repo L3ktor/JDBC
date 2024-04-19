5810d8157679d893c6b37582028358f26d9d218a
L3ktor <goopapota@gmail.com>
2024-04-19 11:14:41 +0300

    Устранил вопросы со скобочками

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -19,9 +19,7 @@ public UserDaoJDBCImpl() {
 public void createUsersTable() throws SQLException {
     String sql = "CREATE TABLE IF NOT EXISTS Users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45) NOT NULL, lastName VARCHAR(45) NOT NULL, age INT NOT NULL)";
 
-    try (Connection connection = Util.getConnection();
-         PreparedStatement preparedStatement = connection.prepareStatement(sql)
-    ) {
+    try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
         preparedStatement.executeUpdate();
     } catch (SQLException e) {
         e.printStackTrace();
@@ -32,9 +30,7 @@ public void createUsersTable() throws SQLException {
 public void dropUsersTable() throws SQLException {
     String sql = "DROP TABLE Users";
 
-    try (Connection connection = Util.getConnection();
-         PreparedStatement preparedStatement = connection.prepareStatement(sql)
-    ) {
+    try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
         preparedStatement.executeUpdate();
     } catch (SQLException e) {
         e.printStackTrace();
@@ -45,9 +41,7 @@ public void dropUsersTable() throws SQLException {
 public void saveUser(String name, String lastName, byte age) throws SQLException {
     String sql = "INSERT INTO USERS(name, lastName,age) VALUES (?,?,?)";
 
-    try (Connection connection = Util.getConnection();
-         PreparedStatement preparedStatement = connection.prepareStatement(sql)
-    ) {
+    try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
         preparedStatement.setString(1, name);
         preparedStatement.setString(2, lastName);
         preparedStatement.setByte(3, age);
@@ -78,9 +72,7 @@ public List<User> getAllUsers() throws SQLException {
     List<User> list = new ArrayList<>();
     String sql = "SELECT * FROM Users";
 
-    try (Connection connection = Util.getConnection();
-         PreparedStatement statement = connection.prepareStatement(sql);
-    ) {
+    try (Connection connection = Util.getConnection(); PreparedStatement statement = connection.prepareStatement(sql)) {
         ResultSet resultSet = statement.executeQuery();
         while (resultSet.next()) {
             long id = resultSet.getLong("id");
@@ -100,9 +92,7 @@ public List<User> getAllUsers() throws SQLException {
 public void cleanUsersTable() throws SQLException {
     String sql = "DELETE FROM Users";      //или "TRUNCATE TABLE Users"
 
-    try (Connection connection = Util.getConnection();
-         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-    ) {
+    try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
         preparedStatement.executeUpdate();
     } catch (SQLException e) {
         e.printStackTrace();
